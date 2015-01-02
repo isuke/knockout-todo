32ae393a624ed7f404a43b37f526115c281f4e04
isuke <isuke770@gmail.com>
2015-01-02 16:52:11 +0900

    Modeling task.

---
+++ db/migrate/20150102074843_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+
+      t.timestamps
+    end
+    add_index :tasks, :title, unique: true
+  end
+end
